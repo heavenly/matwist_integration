2ea299452c018e0211789b10b5bf13e9cf74cba7
moneysake <19937917+moneysake@users.noreply.github.com>
2020-04-16 16:19:14 -0400

    formatted
    formatted, removed some debug messages, and fixed a minor issue i made

--- integration.user.js
+++ integration.user.js
@@ -15,7 +15,6 @@
  * - anime_name = stores the anime_name's current episode
  * - keystore = stores a list of keys -> names of animes that haven't been updated
  * */
-
 function get_keystore() {
     var keystore_values_str = GM_getValue("anime_keystore");
     if (keystore_values_str == null || keystore_values_str == undefined) {
@@ -49,48 +48,49 @@ function is_null_undef(variable) {
 function per_site_basis() {
     if (window.location.href.includes("myanimelist.net")) {
         csrf_token_element = document.getElementsByName("csrf_token")[0]; //updating csrf_token
-      
+
         if (is_null_undef(csrf_token_element))
             return;
-      
+
         GM_setValue("mal_csrf_token", csrf_token_element.content);
         console.log("updated csrf token");
-        
+
         var keystore = get_keystore();
-      
+
         if (is_null_undef(keystore) || keystore.length == 0) {
             console.log("keystore is empty/null/undefined - no anime to update");
             return;
         }
-      
+
         keystore.forEach(function(item, index) {
             if (is_null_undef(item)) {
-                continue;
+                return;
             }
             var episode_number = GM_getValue(item);
             var anime_id = GM_getValue(item + "_id");
             if (is_null_undef(episode_number) || is_null_undef(anime_id)) {
-              continue;
+                return;
             }
             send_post_update_episodes(anime_id, episode_number);
             console.log("updated viewing progress for " + item + "(" + anime_id + ") to episode " + episode_number);
         });
         clear_keystore();
-        
+
     } else if (window.location.href.includes("twist.moe")) {
         var local_anime_data = new match_anime_name_and_ep();
-        
+
         if (is_null_undef(local_anime_data) || local_anime_data.anime_name == "ERROR PARSING NAME") {
-            console.log("data is undefined / null - possibly not an episode page?");
+            //not on an actual anime page
             return;
         }
-      
+
         var myanimelist_id = get_current_page_mal_id();
         var current_episode = local_anime_data.anime_episode;
-      
-        if (is_null_undef(myanimelist_id))
+
+        if (is_null_undef(myanimelist_id)) {
             return;
-      
+        }
+
         GM_setValue(local_anime_data.anime_name, current_episode);
         append_to_keystore(local_anime_data.anime_name);
         console.log("updated viewing progress. logon to MAL")
@@ -110,12 +110,12 @@ function match_anime_name_and_ep() {
     var current_url = window.location.href;
     var regex_fn = /https:\/\/twist.moe\/a\/(.*)\/(\d+)/; //https://regex101.com/r/xRsH4u/1/
     var matches = current_url.match(regex_fn);
-  
+
     if (is_null_undef(matches)) {
         this.anime_name = "ERROR PARSING NAME";
         return;
     }
-  
+
     matches.splice(0, 1); //remove the current_url match  
     this.anime_name = matches[0];
     this.anime_episode = parseInt(matches[1]);
@@ -123,17 +123,17 @@ function match_anime_name_and_ep() {
 
 function get_current_page_mal_id() {
     var local_anime_data = new match_anime_name_and_ep();
-    
+
     if (is_null_undef(local_anime_data) || local_anime_data.anime_name == "ERROR PARSING NAME")
-        return;
-  
+        return null;
+
     var anime_id = GM_getValue(local_anime_data.anime_name + "_id");
     if (is_null_undef(anime_id)) {
         //alert("anime id is null, open console for details");
         var user_response = parseInt(prompt("enter this anime's MAL id"));
         if (is_null_undef(user_response) || isNaN(user_response)) {
             console.clear();
-            console.log("use set_current_anime_id(myanimelist anime's id) to setup id for this page, or insert something normal into the textbox (by refreshing the page)"); 
+            console.log("use set_current_anime_id(myanimelist anime's id) to setup id for this page, or insert something normal into the textbox (by refreshing the page)");
             return null;
         }
         set_current_anime_id(user_response);
@@ -145,15 +145,15 @@ function get_current_page_mal_id() {
 function send_post_update_episodes(id, episode) {
     var csrf_token = GM_getValue("mal_csrf_token");
     var settings = {
-    "url": "https://myanimelist.net/ownlist/anime/edit.json",
-    "method": "POST",
-    "timeout": 0,
-    "data": "{\"anime_id\":" + id + ",\"status\":1,\"score\":0,\"num_watched_episodes\":" + episode + ",\"csrf_token\":\"" + csrf_token + "\"}",
-  };
+        "url": "https://myanimelist.net/ownlist/anime/edit.json",
+        "method": "POST",
+        "timeout": 0,
+        "data": "{\"anime_id\":" + id + ",\"status\":1,\"score\":0,\"num_watched_episodes\":" + episode + ",\"csrf_token\":\"" + csrf_token + "\"}",
+    };
 
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-  });
+    $.ajax(settings).done(function(response) {
+        console.log(response);
+    });
 }
 
 per_site_basis();
